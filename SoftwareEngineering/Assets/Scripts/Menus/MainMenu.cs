b7b34b2d2569235ffa3297242346c24ae5524ff6
Byron <byronandrews-salvietto@live.co.uk>
2019-02-16 12:11:38 +0000

    added menus folder and began main menu

---
+++ SoftwareEngineering/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    //three functions for the menu
+
+
+
+    // one to bring up the settings
+    void OpenSettings()
+    {
+
+    }
+
+    //one to begin the game
+    void BeginGame()
+    {
+
+    }
+
+    //one to load differnt floors of game
+    void LoadFloorGame()
+    {
+
+    }
+
+
+}
