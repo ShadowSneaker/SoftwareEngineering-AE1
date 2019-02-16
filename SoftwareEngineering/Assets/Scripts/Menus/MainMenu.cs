4ce925a5d0b69aa95b7e13d11c408708c4ca3c86
Byron <byronandrews-salvietto@live.co.uk>
2019-02-16 14:51:41 +0000

    Main Menu
    Begin game button complete

--- SoftwareEngineering/Assets/Scripts/Menus/MainMenu.cs
+++ SoftwareEngineering/Assets/Scripts/Menus/MainMenu.cs
@@ -1,31 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator CameraAnim;
+    public int waitTime;
+
+    public string BeginingScene;
 
     //three functions for the menu
 
 
 
     // one to bring up the settings
-    void OpenSettings()
+    public void OpenSettings()
     {
 
     }
 
     //one to begin the game
-    void BeginGame()
+    public void BeginGame()
     {
+        //CameraAnim = FindObjectOfType<Animator>();
+        CameraAnim.SetBool("BeginGame", true);
 
+        //then the code to load the scene
+        StartCoroutine(LoadScene(BeginingScene));
+        
     }
 
     //one to load differnt floors of game
-    void LoadFloorGame()
+    public void LoadFloorGame()
     {
 
     }
 
+
+
+    IEnumerator LoadScene(string SceneName)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(SceneName);
+    }
 
 }
