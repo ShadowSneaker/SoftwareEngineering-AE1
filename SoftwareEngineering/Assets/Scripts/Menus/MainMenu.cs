30b8d28e0a274120f0a68bd39706e2817d9a18cf
Herobyron <Byronandrews-salvietto@live.co.uk>
2019-02-17 15:05:06 +0000

    Main menu stuff
    all buttons for main menu made

--- SoftwareEngineering/Assets/Scripts/Menus/MainMenu.cs
+++ SoftwareEngineering/Assets/Scripts/Menus/MainMenu.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class MainMenu : MonoBehaviour
 
     public string BeginingScene;
 
+    public Image Settings;
+    public Image FloorSelect;
+
     //three functions for the menu
 
 
@@ -17,7 +21,8 @@ public class MainMenu : MonoBehaviour
     // one to bring up the settings
     public void OpenSettings()
     {
-
+        // makes the settings panel appear
+        Settings.gameObject.SetActive(true);
     }
 
     //one to begin the game
@@ -34,10 +39,21 @@ public class MainMenu : MonoBehaviour
     //one to load differnt floors of game
     public void LoadFloorGame()
     {
-
+        // this will have the pannel appear
+        FloorSelect.gameObject.SetActive(true);
     }
 
+    public void CloseLoadFloorPanel()
+    {
+        // makes the load floor panel disapear
+        FloorSelect.gameObject.SetActive(false);
+    }
 
+    public void CloseSettingsPanel()
+    {
+        // makes the settings pannel disapear
+        Settings.gameObject.SetActive(false);
+    }
 
     IEnumerator LoadScene(string SceneName)
     {
