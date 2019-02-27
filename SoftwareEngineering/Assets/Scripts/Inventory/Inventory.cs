6a461d63b1f348177d1e8fe2e57a6201bf18d53e
Byron <byronandrews-salvietto@live.co.uk>
2019-02-27 13:35:00 +0000

    Inventory and Other models
    Inventroy has been added to player]
    Inventory UI Added to Canvas
    Models added to folder for other enemys (GraveDigger and ghost)

---
+++ SoftwareEngineering/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public static Inventory Instance;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogWarning("More then one instance of inventory found");
+        }
+
+        Instance = this;
+    }
+
+    // uncomment once items have been done
+    // public List<ItemScript> Items = new List<ItemScript>();
+    public int space;
+    public delegate void OnItemChanged();
+    public OnItemChanged OnItemChangedCallBack;
+
+
+    // uncomment once items have been done
+    //  public bool AddItems (ItemScript item)
+    //  {
+    //      if(AddItems.Count >= space)
+    //      {
+    //          Debug.Log("No Room My good friend");
+    //          return false
+    //      }
+    //  
+    //      AddItems.Add(item);
+    //  
+    //  
+    //      if(OnItemChangedCallBack != null)
+    //      {
+    //          OnItemChangedCallBack.Invoke();
+    //      }
+    //  
+    //  }
+    //  
+    //  public void RemoveItems(ItemScipt item)
+    //  {
+    //      AddItems.Remove(item);
+    //  
+    //      if (OnItemChangedCallBack != null)
+    //      {
+    //          OnItemChangedCallBack.Invoke();
+    //      }
+    //  }
+
+
+
+}
